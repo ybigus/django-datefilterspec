f4c736570e5dd44717453cf2cb37eb12104b2bbf
Tomas Zulberti <tzulberti@desktop.(none)>
2012-07-03 00:30:47 -0300

    Added basic setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,40 @@
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup, find_packages
-
+from setuptools import setup, find_packages
 
 setup(
     name='django-daterange-filter',
     version='0.0.1',
-    description='',
-    author='',
-    author_email='',
-    url='',
+    description='Allow to filter by a custom date range on the Django Admin',
+    long_description=open('README').read(-1),
+    author='Tomas Zulberti',
+    author_email='tzulberti@gmail.com',
+    url='http://github.com/tzulberti/django-datefilterspec',
+    keywords=[
+        'django admin',
+        'django date range',
+    ],
     install_requires=[
         "Django",
         "South",
     ],
-    packages= find_packages(),
+    packages=find_packages(),
     include_package_data=True,
-    test_suite='nose.collector',
     zip_safe=False,
-    entry_points="""
-""",
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Framework :: Django',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Environment :: Web Environment',
+        'Operating System :: OS Independent'
+    ],
+    license = 'License :: OSI Approved :: BSD License',
 )
