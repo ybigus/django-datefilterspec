1f993330d0db7cefc5ccb1ccc251a3d6f17919dd
Tomas Zulberti <tzulberti@desktop.(none)>
2012-07-11 21:28:34 -0300

    Nuevo release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-daterange-filter',
-    version='0.0.1',
+    version='0.0.2',
     description='Allow to filter by a custom date range on the Django Admin',
     long_description=open('README').read(-1),
     author='Tomas Zulberti',
