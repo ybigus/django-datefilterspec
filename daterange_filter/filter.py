717a8756c99a0a8e9b2dee4d84b3f12ecdf7a9e7
Rinat Shigapov <rinatshigapov@gmail.com>
2012-08-17 13:38:05 +0400

    daterange_filter refactored
    Conflicts:
    
    	daterange_filter/fields.py
    	daterange_filter/filter.py

--- daterange_filter/filter.py
+++ daterange_filter/filter.py
@@ -5,68 +5,60 @@
 Has the filter that allows to filter by a date range.
 
 '''
-from django.utils.translation import ugettext as _
+from django import forms
 from django.contrib import admin
 from django.contrib.admin.widgets import AdminDateWidget
-from django import forms
-from django.conf import settings
-from django.utils.safestring import mark_safe
+from django.db import models
+from django.utils.translation import ugettext as _
 
-class DateForm(forms.Form):
+
+class DateRangeForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
         field_name = kwargs.pop('field_name')
-        super(DateForm, self).__init__(*args, **kwargs)
-        self.fields['%s__gte' % field_name ] = forms.DateField(label=(_('From')),
-                                      widget=AdminDateWidget,
-                                      required=False,
-                                      input_formats=['%Y-%m-%d'])
-        self.fields['%s__lte' % field_name ] = forms.DateField(label=(_('To')),
-                                      widget=AdminDateWidget,
-                                      required=False,
-                                      input_formats=['%Y-%m-%d'])
-        for k in kwargs.get('initial',{}):
-            if not self.fields.has_key(k):
-                self.fields[k] = forms.CharField(widget=forms.HiddenInput())
-                self.fields[k].inital = kwargs.get('initial').get(k)
+        super(DateRangeForm, self).__init__(*args, **kwargs)
+
+        self.fields['%s__gte' % field_name] = forms.DateField(
+            label='', widget=AdminDateWidget(
+                attrs={'placeholder': _('From date')}), localize=True,
+            required=False)
+
+        self.fields['%s__lte' % field_name] = forms.DateField(
+            label='', widget=AdminDateWidget(
+                attrs={'placeholder': _('To date')}), localize=True,
+            required=False)
 
 
-class DateFilterSpec(admin.filters.DateFieldListFilter):
+class DateRangeFilter(admin.filters.FieldListFilter):
+    template = 'daterange_filter/filter.html'
 
-    def __init__(self, f, request, params, model, model_admin, field_path):
-        super(DateFilterSpec, self).__init__(f, request, params, model,
-                                             model_admin, field_path)
-        self.field_generic = '%s__' % field_path
-        self.title = self._title()
+    def __init__(self, field, request, params, model, model_admin, field_path):
+        self.lookup_kwarg_since = '%s__gte' % field_path
+        self.lookup_kwarg_upto = '%s__lte' % field_path
+        super(DateRangeFilter, self).__init__(
+            field, request, params, model, model_admin, field_path)
+        self.form = self.get_form(request)
 
     def choices(self, cl):
-        return [('A', 'B'),]
+        return []
 
-    def _title(self):
-        form = DateForm(initial=self.date_params, field_name=self.field_path)
-        out =  u"""%(field_name)s
-        <style>
-            .calendarbox {
-                left:-400 !important;
-                z-index:1100;
-        }
-        </style>
-        <form method="GET" action="">
-        <ul>
-        %(form)s
-        <li> <input type="submit" value="%(search)s"> </li>
-        </ul>
-        </form>
-        """ % {'search':_('Search'),
-               'form_media': form.media,
-               'form': form.as_p(),
-               'admin_media': settings.STATIC_URL,
-               'field_name': self.field.verbose_name,
-            }
-        return mark_safe(out)
+    def expected_parameters(self):
+        return [self.lookup_kwarg_since, self.lookup_kwarg_upto]
+
+    def get_form(self, request):
+        return DateRangeForm(data=self.used_parameters,
+                             field_name=self.field_path)
+
+    def queryset(self, request, queryset):
+        if self.form.is_valid():
+            # get no null params
+            filter_params = dict(filter(lambda x: bool(x[1]),
+                                        self.form.cleaned_data.items()))
+            return queryset.filter(**filter_params)
+        else:
+            return queryset
 
 
 # register the filter
-admin.filters.FieldListFilter._field_list_filters.insert(0, (
-                                lambda f: getattr(f, 'daterange_filter', False),
-                                DateFilterSpec))
+admin.filters.FieldListFilter.register(
+    lambda f: isinstance(f, models.DateField), DateRangeFilter)
